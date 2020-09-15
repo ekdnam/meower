d99085d4c005850de1844ed63aa840eb9e194e3b
ekdnam <adeetya.m@gmail.com>
2020-09-15 18:39:50 +0530

    add npm package cors

--- client/client.js
+++ client/client.js
@@ -3,6 +3,7 @@ console.log("Hello World");
 // document means client-side js
 const form = document.querySelector('form');
 const loadingElement = document.querySelector('.loading');
+const API_URL = "http://localhost:5000/mews";
 
 loadingElement.style.display = "none";
 form.addEventListener('submit', (event) => {
@@ -20,4 +21,12 @@ form.addEventListener('submit', (event) => {
     form.style.display = 'none';
     console.log(mew);
     loadingElement.style.display = '';
+
+    fetch(API_URL, {
+        method: 'POST',
+        body: JSON.stringify(mew),
+        headers: {
+            'content-type': 'application/json'
+        }
+    });
 });
