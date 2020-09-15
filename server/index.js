f32b1e4fd690ff757968ef3520c5dc6d5afa5b65
ekdnam <adeetya.m@gmail.com>
2020-09-15 18:52:44 +0530

    add server side validation

--- server/index.js
+++ server/index.js
@@ -13,8 +13,24 @@ app.get("/", (req, res) => {
     });
 });
 
+function isValidMew(mew) {
+    return mew.name && mew.name.toString().trim() !== '' && mew.content && mew.content.toString().trim() !== '';
+}
+
 app.post('/mews', (req, res) => {
-    console.log(req.body);
+    if (isValidMew(req.body)) {
+        // insert into db
+        const mew = {
+            name: req.body.name.toString(),
+            content: req.body.content.toString()
+        };
+        console.log(mew);
+    } else {
+        res.status(422);
+        res.json({
+            message: "Name and content are reqd"
+        })
+    }
 });
 app.listen(5000, () => {
     console.log("Listening on http://localhost:5000");
