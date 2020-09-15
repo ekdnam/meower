66f2bbb29caf144248831b0b937ec4b04303e68c
ekdnam <adeetya.m@gmail.com>
2020-09-15 18:21:35 +0530

    add nodemon dependency

--- server/index.js
+++ server/index.js
@@ -3,6 +3,12 @@ const express = require('express');
 // create express application
 const app = express();
 
+app.get("/", (req, res) => {
+    res.json({
+        message: "Meower!"
+    })
+});
+
 app.listen(5000, () => {
     console.log("Listening on http://localhost:5000");
 });
