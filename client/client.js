268870ec3ceea74dd78350f50aa10c79ce92cf38
ekdnam <adeetya.m@gmail.com>
2020-09-15 18:02:00 +0530

    add loading.gif
    when form is submitted, a loading gif appears

--- client/client.js
+++ client/client.js
@@ -2,7 +2,9 @@ console.log("Hello World");
 
 // document means client-side js
 const form = document.querySelector('form');
+const loadingElement = document.querySelector('.loading');
 
+loadingElement.style.display = "none";
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     const formData = new FormData(form);
@@ -15,5 +17,7 @@ form.addEventListener('submit', (event) => {
         content
     }
 
+    form.style.display = 'none';
     console.log(mew);
+    loadingElement.style.display = '';
 });
