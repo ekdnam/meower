18a7c057bd066a4020cb53f28f7d10455dcc674e
ekdnam <adeetya.m@gmail.com>
2020-09-15 17:53:15 +0530

    add form event in client.js

--- client/client.js
+++ client/client.js
@@ -1,3 +1,19 @@
 console.log("Hello World");
 
+// document means client-side js
 const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const name = formData.get('name');
+    const content = formData.get('content');
+    console.log("form was submitted");
+
+    const mew = {
+        name,
+        content
+    }
+
+    console.log(mew);
+});
