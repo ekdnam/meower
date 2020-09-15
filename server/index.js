597fe18b1f464286bba89e8c7f5be50cd1e06821
ekdnam <adeetya.m@gmail.com>
2020-09-15 18:35:04 +0530

    add GET / route, POST / route
    also logging out req.body

--- server/index.js
+++ server/index.js
@@ -6,9 +6,12 @@ const app = express();
 app.get("/", (req, res) => {
     res.json({
         message: "Meow!"
-    })
+    });
 });
 
+app.post('/mews', (req, res) => {
+    console.log(req.body);
+});
 app.listen(5000, () => {
     console.log("Listening on http://localhost:5000");
 });
